26980a3fe4a9aa40e5a814ae3ef2f425f47e952a
imalanvalencia <cash.alanbo2@gmail.com>
2024-01-20 22:22:15 +0100

    :sparkles: Refined header component and completed the layout for …

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import Header from "@/components/ui/header"
-import Footer from "@/components/ui/footer"
-import CreateTask from '@/components/create-task'
+import Header from "@/components/ui/header";
+import Footer from "@/components/ui/footer";
+import checklist_guy from "@/assets/checklist-guy.svg";
 
 export default function App() {
   return (
-    <div className="flex flex-col px-4 min-w-full min-h-screen">
+    <div className="flex flex-col min-w-full w-[425px] min-h-screen backdrop-blur-lg bg-clip-padding backdrop-filter">
       <Header />
-      <main className="flex-grow place-self-center">
-        <CreateTask />
+      <main className="flex-grow text-center px-6">
+        <section>
+          <img src={checklist_guy} alt="checklist guy" className="mx-auto" />
+          <h2 className="text-xl">What do you want to do today?</h2>
+          <p className="text-lg font-light">Tap + to add your tasks</p>
+        </section>
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
-
-
-
-
-
-
