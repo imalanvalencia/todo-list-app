33e07048087ff4e48551cb07fd5549be6a63e2c7
imalanvalencia <contact@alanvalencia.me>
2024-02-25 19:07:16 +0100

    feat: Mark a task as completed

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -3,10 +3,11 @@ import FlagIcon from '../icons/flag'
 import { Card, CardContent, CardTitle } from '../ui/card'
 import { type ITask } from './tasks'
 import { cn } from '@/lib/utils'
+import { Checkbox } from '../ui/checkbox'
+import { useTasks } from '@/hooks/use-tasks'
+import { type CheckedState } from '@radix-ui/react-checkbox'
 
-interface Props extends Omit<ITask, 'id' | 'description'> {
-  toggleTaskState: () => void
-}
+interface Props extends Omit<ITask, 'description'> {}
 
 const Todo: React.FC<Props> = ({
   title,
@@ -14,13 +15,9 @@ const Todo: React.FC<Props> = ({
   date,
   category,
   priority,
-  completed,
-  toggleTaskState
+  completed
 }: Props) => (
-  <Card
-    className={cn('w-full', completed && 'bg-indigo-500')}
-    onClick={toggleTaskState}
-  >
+  <Card className={cn('w-full select-none', completed && 'bg-indigo-500')}>
     <CardTitle className="text-base ">{title}</CardTitle>
     <CardContent className="w-full inline-flex justify-between text-white text-xs">
       <p className="text-sm">
@@ -40,25 +37,38 @@ const Todo: React.FC<Props> = ({
 )
 
 const Task: React.FC<Props> = ({
+  id,
   title,
   time,
   date,
   category,
   priority,
-  completed,
-  toggleTaskState
+  completed
 }: Props) => {
+  const { changeTaskStatus } = useTasks()
+
+  const handleChange = (checked: CheckedState) => {
+    changeTaskStatus({ id, completed: Boolean(checked) })
+  }
+
   return (
     <li className="bg-neutral-800 w-full min-h-16 flex">
-      <Todo
-        title={title}
-        time={time}
-        date={date}
-        category={category}
-        priority={priority}
-        completed={completed}
-        toggleTaskState={toggleTaskState}
+      <Checkbox
+        id={`task-${id}`}
+        checked={completed}
+        onCheckedChange={handleChange}
+        hidden
       />
+      <label htmlFor={`task-${id}`} className="w-full">
+        <Todo
+          title={title}
+          time={time}
+          date={date}
+          category={category}
+          priority={priority}
+          completed={completed}
+        />
+      </label>
     </li>
   )
 }
